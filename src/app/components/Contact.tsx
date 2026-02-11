aeae6a586eca90faed1713485f0723708fe1b16f
Danindu Navoda <135207728+MrCheesyBytes@users.noreply.github.com>
2026-02-11 14:14:41 +0000

    made some changes

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -39,7 +39,7 @@ export default function Contact() {
       value: 'linkedin.com/in/danindu-navoda',
       href: 'https://www.linkedin.com/in/danindu-navoda/',
       icon: Linkedin,
-      command: 'finger danindu --secure',
+      command: 'linkedin-cli --profile danindu-navoda',
       color: 'text-cyan-400'
     },
 
